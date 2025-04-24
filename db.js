dc2ecebe69b7887c64a415d511808b3826cb4668
E.Shanmuka <162003293+E-Shanmuka@users.noreply.github.com>
2025-04-24 13:58:33 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,20 +1,21 @@
 // db.js
 const { Sequelize } = require('sequelize');
 
-// Remote MySQL connection (InfinityFree)
-const sequelize = new Sequelize('if0_38196313_blogwe', 'if0_38196313', '9951117884', {
-  host: 'sql203.infinityfree.com',
+// Remote MySQL connection (Clever Cloud)
+const sequelize = new Sequelize('byxbpgvhrl5xibxn0wli', 'u1mnzcc7unkxm650', 'UMAsdig94CzpjcRsj1ur', {
+  host: 'byxbpgvhrl5xibxn0wli-mysql.services.clever-cloud.com',
   dialect: 'mysql',
   port: 3306,
   logging: true, // Set to false if you want cleaner logs
   dialectOptions: {
-    connectTimeout: 10000, // Optional: gives it more time to connect
+    connectTimeout: 10000 // Optional: gives more time for initial connection
   }
 });
 
 // Test the database connection
 sequelize.authenticate()
-  .then(() => console.log("✅ Connected to InfinityFree MySQL database..."))
+  .then(() => console.log("✅ Connected to Clever Cloud MySQL database..."))
   .catch(err => console.error("❌ MySQL connection error:", err));
 
 module.exports = sequelize;
+
